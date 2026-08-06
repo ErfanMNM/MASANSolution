cc684384f4db9599709e5bd8ac43f53c2ed59285
Your Name <you@example.com>
2026-08-06 23:43:39 +0700

    dsff

--- MASAN-SERIALIZATION/FMain.Designer.cs
+++ MASAN-SERIALIZATION/FMain.Designer.cs
@@ -60,7 +60,7 @@
             // 
             this.lblStatus.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.lblStatus.Location = new System.Drawing.Point(653, 0);
+            this.lblStatus.Location = new System.Drawing.Point(1531, 0);
             this.lblStatus.Margin = new System.Windows.Forms.Padding(0);
             this.lblStatus.MinimumSize = new System.Drawing.Size(1, 1);
             this.lblStatus.Name = "lblStatus";
@@ -91,12 +91,12 @@
             // 
             this.opClock.Dock = System.Windows.Forms.DockStyle.Fill;
             this.opClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.opClock.Location = new System.Drawing.Point(858, 0);
+            this.opClock.Location = new System.Drawing.Point(1736, 0);
             this.opClock.Margin = new System.Windows.Forms.Padding(0);
             this.opClock.MinimumSize = new System.Drawing.Size(1, 1);
             this.opClock.Name = "opClock";
             this.opClock.Radius = 0;
-            this.opClock.Size = new System.Drawing.Size(166, 39);
+            this.opClock.Size = new System.Drawing.Size(166, 53);
             this.opClock.TabIndex = 1;
             this.opClock.Text = "20/11/2024 17:27:00.000";
             this.opClock.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
@@ -106,7 +106,7 @@
             this.lblAllStatus.FillColor = System.Drawing.Color.Red;
             this.lblAllStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.lblAllStatus.ForeColor = System.Drawing.Color.Yellow;
-            this.lblAllStatus.Location = new System.Drawing.Point(357, 0);
+            this.lblAllStatus.Location = new System.Drawing.Point(1235, 0);
             this.lblAllStatus.Margin = new System.Windows.Forms.Padding(0);
             this.lblAllStatus.MinimumSize = new System.Drawing.Size(1, 1);
             this.lblAllStatus.Name = "lblAllStatus";
@@ -132,12 +132,12 @@
             this.footer.Controls.Add(this.lblStatus, 3, 0);
             this.footer.Controls.Add(this.lblAllStatus, 2, 0);
             this.footer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.footer.Location = new System.Drawing.Point(0, 729);
+            this.footer.Location = new System.Drawing.Point(0, 977);
             this.footer.Margin = new System.Windows.Forms.Padding(0);
             this.footer.Name = "footer";
             this.footer.RowCount = 1;
             this.footer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.footer.Size = new System.Drawing.Size(1024, 39);
+            this.footer.Size = new System.Drawing.Size(1902, 53);
             this.footer.TabIndex = 3;
             this.footer.TagString = null;
             // 
@@ -167,7 +167,7 @@
             this.NavMenu.ShowPlusMinus = false;
             this.NavMenu.ShowRootLines = false;
             this.NavMenu.ShowTips = true;
-            this.NavMenu.Size = new System.Drawing.Size(184, 585);
+            this.NavMenu.Size = new System.Drawing.Size(342, 815);
             this.NavMenu.TabIndex = 0;
             this.NavMenu.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             // 
@@ -179,12 +179,12 @@
             this.BodyPanel.Controls.Add(this.navPanel, 0, 0);
             this.BodyPanel.Controls.Add(this.TabBody, 1, 0);
             this.BodyPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.BodyPanel.Location = new System.Drawing.Point(0, 55);
+            this.BodyPanel.Location = new System.Drawing.Point(0, 73);
             this.BodyPanel.Margin = new System.Windows.Forms.Padding(0);
             this.BodyPanel.Name = "BodyPanel";
             this.BodyPanel.RowCount = 1;
             this.BodyPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.BodyPanel.Size = new System.Drawing.Size(1024, 674);
+            this.BodyPanel.Size = new System.Drawing.Size(1902, 904);
             this.BodyPanel.TabIndex = 2;
             this.BodyPanel.TagString = null;
             // 
@@ -204,7 +204,7 @@
             this.navPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.navPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
             this.navPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
-            this.navPanel.Size = new System.Drawing.Size(184, 674);
+            this.navPanel.Size = new System.Drawing.Size(342, 904);
             this.navPanel.TabIndex = 2;
             this.navPanel.TagString = null;
             // 
@@ -213,7 +213,7 @@
             this.btnAppClose.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAppClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAppClose.ForeColor = System.Drawing.Color.MistyRose;
-            this.btnAppClose.Location = new System.Drawing.Point(2, 635);
+            this.btnAppClose.Location = new System.Drawing.Point(2, 865);
             this.btnAppClose.Margin = new System.Windows.Forms.Padding(2);
             this.btnAppClose.MinimumSize = new System.Drawing.Size(1, 1);
             this.btnAppClose.Name = "btnAppClose";
@@ -232,7 +232,7 @@
             // 
             this.btnMini.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnMini.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnMini.Location = new System.Drawing.Point(2, 587);
+            this.btnMini.Location = new System.Drawing.Point(2, 817);
             this.btnMini.Margin = new System.Windows.Forms.Padding(2);
             this.btnMini.MinimumSize = new System.Drawing.Size(1, 1);
             this.btnMini.Name = "btnMini";
@@ -252,13 +252,13 @@
             this.TabBody.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
             this.TabBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.TabBody.ItemSize = new System.Drawing.Size(0, 1);
-            this.TabBody.Location = new System.Drawing.Point(184, 0);
+            this.TabBody.Location = new System.Drawing.Point(342, 0);
             this.TabBody.MainPage = "";
             this.TabBody.Margin = new System.Windows.Forms.Padding(0);
             this.TabBody.MenuStyle = Sunny.UI.UIMenuStyle.Custom;
             this.TabBody.Name = "TabBody";
             this.TabBody.SelectedIndex = 0;
-            this.TabBody.Size = new System.Drawing.Size(840, 674);
+            this.TabBody.Size = new System.Drawing.Size(1560, 904);
             this.TabBody.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.TabBody.TabBackColor = System.Drawing.Color.PaleTurquoise;
             this.TabBody.TabIndex = 1;
@@ -400,7 +400,7 @@
             this.mainPanelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.161458F));
             this.mainPanelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.76041F));
             this.mainPanelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.mainPanelLayout.Size = new System.Drawing.Size(1024, 768);
+            this.mainPanelLayout.Size = new System.Drawing.Size(1902, 1030);
             this.mainPanelLayout.TabIndex = 1;
             this.mainPanelLayout.TagString = null;
             // 
@@ -408,10 +408,10 @@
             // 
             this.AllowShowTitle = false;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.ClientSize = new System.Drawing.Size(1902, 1030);
             this.Controls.Add(this.mainPanelLayout);
-            this.MaximumSize = new System.Drawing.Size(1024, 768);
-            this.MinimumSize = new System.Drawing.Size(1024, 768);
+            this.MaximumSize = new System.Drawing.Size(1902, 1080);
+            this.MinimumSize = new System.Drawing.Size(1902, 1030);
             this.Name = "FMain";
             this.Padding = new System.Windows.Forms.Padding(0);
             this.ShowTitle = false;
